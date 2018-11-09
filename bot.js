4d9edaecb9c6c7135bd11df9e2fdcf58642e94a5
` 0ff <39999036+00F@users.noreply.github.com>
2018-11-10 00:22:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,32 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const developers = ['343743154429755392'];
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("If you can dream it you can do it",{type: 'LISTENING'})
+  client.user.setStatus("dnd")
+});
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
+
+if (message.content.startsWith('.2')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.send(`${argresult} \` `)
+} else 
+if (message.content.startsWith('.1')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+    message.channel.send(` ${argresult} \` `)
+} else 
+if (message.content.startsWith('.3')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/MeeRcY");
+   message.channel.send(`${argresult} \` `)
+}
+});
+
 client.on('message', message => {
         if (!developers.includes(message.author.id)) return;
   if (message.content === '712') {
@@ -13,10 +39,4 @@ client.on('message', message => {
   }
           
 });
-
-
-
-
-
-
 client.login(process.env.BOT_TOKEN);
